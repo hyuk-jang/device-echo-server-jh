fb88411c065622461027fe88461a530e6a18821d
장혁 <hyuk.jang@smsoft.co.kr>
2019-05-09 18:12:41 +0900

    @ UPSAS 에코서버 타이밍 수정

---
+++ [에코서버] Nodemon.bat
@@ -0,0 +1,1 @@
+nodemon "./test/UPSAS/xbee/socketServer.js"
--- src/UPSAS/xbee/EchoServer.js
+++ src/UPSAS/xbee/EchoServer.js
@@ -18,8 +18,8 @@ class EchoServer extends Model {
 
     this.bufDataBattery = Buffer.from([0x31, 0x30, 0x2e, 0x32]);
 
-    this.normalDeviceOperTime = 300;
-    this.pumpDeviceOperTime = 500;
+    this.normalDeviceOperTime = 30;
+    this.pumpDeviceOperTime = 50;
   }
 
   /**
